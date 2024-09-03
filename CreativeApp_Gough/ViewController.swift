59c7e2629647a81e367e7d4e7fa39d5cbb4d5eb8
Logan <logan.gough26@d155.org>
2024-09-03 08:03:38 -0500

    fnifhsed

--- CreativeApp_Gough/ViewController.swift
+++ CreativeApp_Gough/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ageField: UITextField!
     
+    
+    @IBOutlet weak var rankLabel: UILabel!
+    
+    
     var champion = 100
     var diamond = 120
     var gold = 140
@@ -27,8 +31,59 @@ class ViewController: UIViewController {
 
         var score =  scoreField.text!
         var frog = Int(score) ?? 0
+        var age = ageField.text!
+        var cat = Int(age) ?? 0
+        if(cat < 10){
+            frog = frog - 15
+        }
+        else if(cat == 11){
+            frog = frog - 10
+        }
+        else if(cat == 12){
+            frog = frog - 5
+        }
+        else if(cat == 13){
+            frog = frog - 1
+        }
+        else if(cat == 0){
+            view.backgroundColor = UIColor.black
+            rankLabel.text = "PLEASE ENTER AGE"
+        }
+        
+        
+        
         if(frog < 120){
-            
+            rankLabel.text = "Your a CHAMPION"
+            view.backgroundColor = UIColor.purple
+        }
+        else if(frog >= 120 && frog < 140){
+            rankLabel.text = "Your in diamond"
+            view.backgroundColor = UIColor.blue
+        }
+        else if(frog >= 140 && frog < 160){
+            rankLabel.text = "Your in gold"
+            view.backgroundColor = UIColor.yellow
+        }
+        else if(frog >= 160 && frog < 180){
+            rankLabel.text = "your in silver"
+            view.backgroundColor = UIColor.gray
+        }
+        else if(frog >= 180 && frog < 200){
+            rankLabel.text = "your in bronze"
+            view.backgroundColor = UIColor.brown
+        }
+        else if(frog == 0){
+            rankLabel.text = "Your unranked"
+            view.backgroundColor = UIColor.black
+        }
+        else{
+            rankLabel.text = "Your unranked"
+            view.backgroundColor = UIColor.white
+        }
+        
+         if(cat == 0){
+            view.backgroundColor = UIColor.white
+            rankLabel.text = "PLEASE ENTER AGE"
         }
         
     }
@@ -38,7 +93,6 @@ class ViewController: UIViewController {
             UIApplication.shared.open(url)
         }
     }
-    
     
 }
 
